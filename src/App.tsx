40dbe3c94bdd42b4f0e28f1ddb267b2311eb71fe
Marcos Hayama <marcos.hayama@gmail.com>
2024-11-02 18:40:55 -0300

    feature/qr-code-component

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+  <div className="main-box">
+    <div className="box">
+      <div className="box-header">
+        <img src="../public/qr-image.jpg"/>
+      </div>
+      <div className="box-content">
+        <h1 className="box-title">Improve your front-end skills by building projects</h1>
+        <h2 className="box-subtitle">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h2>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
